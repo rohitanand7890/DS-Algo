8377f041c864e2cc7bdc3d0e6fafbe650c37bd1b
rohitanand7890 <rohitanand650.com>
2017-07-20 23:02:16 +0530

    testing java

---
+++ src/com/rohit/Main.java
@@ -0,0 +1,54 @@
+package com.rohit;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+	// write your code here
+        /**
+         * this
+         * project
+         * beloin
+         *
+          */
+       //todo
+
+        //psvm
+        //sout
+        int i =0 ;
+        i++;
+        i++;
+        System.out.println(i);
+        int j = i+100;
+        // Ctrl + Z  - undo
+        // Ctrl +Shift + Z - redo
+        System.out.println(j);
+        System.out.print(" -------- "); // new line
+        System.out.println("sum of i and j "+ (i+j));
+        run(); //Ctrl + click.
+
+        System.out.println("hello world");
+        new Main().runIt();
+
+
+
+    }
+    //private
+    //void ??? String, int, ArrayList<String>
+    //  static method
+    private static void run(){
+        System.out.println("running...");
+    }
+
+    // non static method
+    private  void runIt(){
+        System.out.println("running runit...");
+    }
+
+    private  String It(){
+        System.out.println("running runit...");
+        return "str";
+    }
+
+}
